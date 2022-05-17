6d81ca5a4658428fcbcaa822692020a636eb67da
soydignelson <105730839+soydignelson@users.noreply.github.com>
2022-05-17 12:07:20 -0300

    digBOT
    digBOT

---
+++ logos/varmsgbot.js
@@ -0,0 +1,15 @@
+["Boa tarde amores 💖"
+
+,"Boa tarde amig(a) 😉"
+
+,"Boa tarde lindo(a) 😍"
+
+,"Boa tarde zé lutão 😋"
+
+,"Boa tarde família 🥰"
+
+,"Boa tarde esquadremo 🤠"
+
+,"Boa tarde filho(a) de Deus ❤"
+
+,"Boa tarde apoiador(a) do dig na loja 👓"]
